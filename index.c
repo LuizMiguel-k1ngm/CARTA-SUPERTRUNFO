30db07b54ef1d71f9362c53191066dbfd4de7e8b
Luiz Miguel <140824165+LuizMiguel-k1ngm@users.noreply.github.com>
2025-04-07 22:36:26 -0300

    Update 2.0 index.c

--- index.c
+++ index.c
@@ -14,8 +14,11 @@ int main() {
 	char estado1[3], estado2[3];
 	char cod1[50], cod2[50], nomeCidade1[50], nomeCidade2[50];
 	float  area1, area2, pib1, pib2;
-	int pontos1, pontos2, populacao1, populacao2;
+	int pontos1, pontos2;
+	signed long int populacao1, populacao2;
 	float cdp1, cdp2, cpc1, cpc2; 
+	int p1 = 0, p2 =0;
+	float superPoder;
 	
 	
 	
@@ -86,6 +89,82 @@ printf("---------------------------------------------- \n");
 	
 	
 	
+	/*----------------------------------------------------------------------------------------------*/
+	//criação da lógica
+	
+	
+	//comparando POPULAÇÃO
+	
+	if (populacao1 > populacao2){
+		p1++;
+	}else{
+		p2++;
+	}
+	
+	
+	//comparando AREA
+	
+	if(area1 > area2){
+		p1++;
+	}else{
+		p2++;
+	}
+	
+	//comparando PIB
+	if(pib1 > pib2){
+		p1++;
+	}else{
+		p2++;
+	}
+	
+	
+	//comparando pib per capita
+	
+	if(cpc1 > cpc2){
+		p1++;
+	}else{
+		p2++;
+	}
+	
+	
+	//comparando densidade populacional invertida
+	if((1/cdp1) > (1/cdp2)){
+		p1++;
+	}else{
+		p2++;
+	}
+	
+	
+	//comparando pontos turisticos
+	if(pontos1 > pontos2){
+		p1++;
+	}else{
+		p2++;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	//SuperPoder
+	if((populacao1+area1+pib1+cpc1+cdp1+pontos1) > (populacao2+area2+pib2+cpc2+cdp2+pontos2)){
+		p1++;
+	}else{
+		p2++;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	printf("---------------------------------------------- \n");
@@ -96,12 +175,27 @@ printf("---------------------------------------------- \n");
 	
 	
 	printf("CARTA 1\n");
-	printf("O estado : %s\ncodigo 1: %s\nCidade: %s\nPopulacao: %i\nArea:%.2f km²\nPIB: %.2f bilhoes de reais\nNúmeros de pontos turisticos: %i\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f reais\n ", estado1, cod1, nomeCidade1, populacao1,area1, pib1, pontos1, cdp1,cpc1 );
+	printf("O estado : %s\ncodigo 1: %s\nCidade: %s\nPopulacao: %lu\nArea:%.2f km²\nPIB: %.2f bilhoes de reais\nNúmeros de pontos turisticos: %i\nDensidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f reais\n ", estado1, cod1, nomeCidade1, populacao1,area1, pib1, pontos1, cdp1,cpc1 );
 	
 	printf("---------------------------------------------- \n");
 	
 	printf("CARTA 2\n");
-	printf("O estado: %s\ncodigo: %s\nCidade:  %s\nPopulacao: %i\nArea:%.2f km²\nPIB: %.2f bilhoes de reais\nNumeros de pontos turisticos: %i\nDensidade PopulacionaL: %.2f hab/km²\nPIB per Capita %.2f reais\n ", estado2, cod2, nomeCidade2, populacao2, area2, pib2, pontos2, cdp2, cpc2);
+	printf("O estado: %s\ncodigo: %s\nCidade:  %s\nPopulacao: %lu\nArea:%.2f km²\nPIB: %.2f bilhoes de reais\nNumeros de pontos turisticos: %i\nDensidade PopulacionaL: %.2f hab/km²\nPIB per Capita %.2f reais\n ", estado2, cod2, nomeCidade2, populacao2, area2, pib2, pontos2, cdp2, cpc2);
+	
+
+
+	printf("---------------------------------------------- \n");
+	
+
+
+	
+	if (p1 > p2) {
+        printf("Carta 1 VENCEU!\n");
+    } else if (p2 > p1) {
+        printf("Carta 2 VENCEU!\n");
+    } else {
+        printf("EMPATE!\n");
+    }
 	
 	
 	
@@ -111,9 +205,7 @@ printf("---------------------------------------------- \n");
 	
 	
 	
-	
-	
-	
+
 	
 	
 	return 0;
